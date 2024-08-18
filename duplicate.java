adc1b0d58dfcdfbdafe0ea6a264eec6b0cc295a8
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-08-18 11:28:57 +0530

    duplicate array

---
+++ duplicate.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class duplicate {
+
+    public static void findDuplicates(int ar[])
+    {
+        //hashmap only allows unique key to insert
+        HashMap<Integer,Integer> lst = new HashMap<>();
+        for(int i=0;i<ar.length;i++)
+        {
+            for(int j=i+1;j<ar.length;j++)
+            {
+                if(ar[i]==ar[j] )
+                {
+                    lst.put(ar[i], ar[i]);
+                    
+                }
+            }
+        }
+
+       System.out.println(lst.values());
+        
+    }
+    
+    public static void main(String[] args) {
+        int ar[]={1,2,3,1,2,2,2,3,4,5,6756,342,1,4,6756,12};
+        findDuplicates(ar);     
+    }
+}
