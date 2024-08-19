2b6a2be33ed4c9a25c625a7bf96b7009ee5cd704
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-08-19 10:35:31 +0530

    array programs

---
+++ evenele.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class evenele {
+
+    public static void main(String[] args) {
+        int ar[] = {1,2,3,4,5};
+        int k=1;
+        for(int i=0;i<ar.length;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(ar[i]);
+            }
+        }
+    }
+    
+}
---
+++ oddele.java
@@ -0,0 +1,13 @@
+public class oddele {
+    public static void main(String[] args) {
+        int ar[] = {1,2,3,4,5};
+        int k=1;
+        for(int i=0;i<ar.length;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(ar[i]);
+            }
+        }
+    }
+}
